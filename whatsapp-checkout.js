d43fbea1bfa10fd72076fc8ad7a063ab1820ea0e
emmagonn <emmagon.4@gmail.com>
2025-06-12 12:52:46 -0300

    Update whatsapp-checkout.js

--- whatsapp-checkout.js
+++ whatsapp-checkout.js
@@ -1,11 +1,8 @@
 (function () {
   const observer = new MutationObserver(() => {
-    const title = Array.from(document.querySelectorAll("h3, h2, strong")).find(el =>
-      el.textContent.trim().toLowerCase().startsWith("datos para transferencia")
-    );
-
-    if (title && !title.innerHTML.includes("wa.me")) {
-      title.innerHTML = 'Datos para transferencia - <a href="https://wa.me/5491123872258" target="_blank" style="color:#007bff;text-decoration:underline;">Whatsapp</a>';
+    const el = document.querySelector('h4.order_card-info-title');
+    if (el && !el.innerHTML.includes("wa.me")) {
+      el.innerHTML = 'Datos para transferencia - <a href="https://wa.me/5491123872258" target="_blank" style="color:#007bff;text-decoration:underline;">Whatsapp</a>';
       observer.disconnect();
     }
   });
